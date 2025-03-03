ff5efd30526320a0e7fa9de94c6c40b599aba13c
Trallkong <trallkongn@petalmail.com>
2025-03-03 19:41:13 +0800

    2025-3-3

---
+++ premake5.lua
@@ -0,0 +1,107 @@
+workspace "Azer"
+  architecture "x64"
+
+  configurations
+  {
+    "Debug",
+    "Release",
+    "Dist"
+  }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Azer"
+  location "Azer"
+  kind "SharedLib"
+  language "C++"
+  targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+  objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+  files
+  {
+    "%{prj.name}/src/**.h",
+    "%{prj.name}/src/**.cpp"
+  }
+
+  includedirs
+  {
+    "%{prj.name}/vender/spdlog/include"
+  }
+
+  filter "system:windows"
+    cppdialect "C++17"
+    staticruntime "On"
+    systemversion "10.0.17134.0"
+
+    defines
+    {
+      "AZ_PLATFORM_WINDOWS",
+      "AZ_BUILD_DLL"
+    }
+
+    postbuildcommands
+    {
+      ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+    }
+
+  filter "configurations:Debug"
+    defines "AZ_DEBUG"
+    symbols "On"
+
+  filter "configurations:Release"
+    defines "AZ_RELEASE"
+    symbols "On"
+
+  filter "configurations:Dist"
+    defines "AZ_DIST"
+    symbols "On"
+  
+  files {"system:windows","configurations:Release"}
+    buildoptions "/MT"
+  
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+  files
+  {
+    "%{prj.name}/src/**.h",
+    "%{prj.name}/src/**.cpp"
+  }
+
+  includedirs
+  {
+    "%{prj.name}/vender/spdlog/include",
+    "Azer/src"
+  }
+
+  links
+  {
+    "Azer"
+  }
+
+  filter "system:windows"
+    cppdialect "C++17"
+    staticruntime "On"
+    systemversion "10.0.17134.0"
+
+    defines
+    {
+      "AZ_PLATFORM_WINDOWS",
+    }
+
+  filter "configurations:Debug"
+    defines "AZ_DEBUG"
+    symbols "On"
+
+  filter "configurations:Release"
+    defines "AZ_RELEASE"
+    symbols "On"
+
+  filter "configurations:Dist"
+    defines "AZ_DIST"
+    symbols "On"
