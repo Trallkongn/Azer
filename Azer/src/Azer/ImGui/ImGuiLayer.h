e673741cd061571693238942719f03ed93eb6f19
Trallkong <trallkongn@petalmail.com>
2025-06-17 18:29:29 +0800

    20250617

--- Azer/src/Azer/ImGui/ImGuiLayer.h
+++ Azer/src/Azer/ImGui/ImGuiLayer.h
@@ -12,29 +12,14 @@ namespace Azer {
 	{
 	public:
 		ImGuiLayer();
-
 		~ImGuiLayer();
 
+		virtual void OnAttach() override;
+		virtual void OnDetach() override;
+		virtual void OnImGuiRender() override;
 
-		void OnUpdate() override;
-
-
-		void OnEvent(Event& event) override;
-
-
-		void OnAttach() override;
-
-
-		void OnDetach() override;
-	private:
-		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
-		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
-		bool OnMouseMovedEvent(MouseMovedEvent& e);
-		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
-		bool OnKeyPressedEvent(KeyPressedEvent& e);
-		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
-		bool OnWindowResizeEvent(WindowResizeEvent& e);
-		bool OnKeyTypedEvent(Event& e);
+		void Begin();
+		void End();
 	private:
 		float m_Time = 0.0f;
 	};
